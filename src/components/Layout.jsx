8a2fc98470f0e44476afdaa6d7a1b702447af2fd
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-05-17 21:39:13 +0300

    Update Layout component for conditional rendering and component composition
    This commit enhances the Layout component to efficiently manage the rendering of different UI components based on the state of the application. It introduces conditional rendering logic to display either the MovieList component or the MovieDetails, WatchedSummary, and WatchedMovieList components based on the selectedId state. Additionally, it includes error handling to display an Error component when an error occurs during data fetching. These changes improve the overall structure and user experience of the application layout.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,15 +6,27 @@ import { useMovies } from '../context/MoviesContext';
 import MovieList from './MovieList';
 import Error from './Error';
 import Loader from './Loader';
+import MovieDetails from './MovieDetails';
+import WatchedSummary from './WatchedSummary';
+import WatchedMovieList from './WatchedMovieList';
 
 function Layout() {
-  const { error, isLoading } = useMovies();
+  const { error, isLoading, selectedId } = useMovies();
   return (
     <>
       <Navbar />
       <Main>
         <Box>{error ? <Error message={error} /> : !error && isLoading ? <Loader /> : <MovieList />}</Box>
-        <Box></Box>
+        <Box>
+          {selectedId ? (
+            <MovieDetails />
+          ) : (
+            <>
+              <WatchedSummary />
+              <WatchedMovieList />
+            </>
+          )}
+        </Box>
       </Main>
     </>
   );
